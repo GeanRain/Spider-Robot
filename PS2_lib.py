adaefd92d1cb350cd1da656bf291cc19731fefd9
GeanRain <geanrain@gmail.com>
2019-03-05 18:01:41 +0800

    - cancle cmdList[] - add more note - add setConfig() in PS2GamePad class - add setAnalogMode() in PS2GamePad class - add setVibMode() in PS2GamePad class - rebuild setVibValue() in PS2GamePad class - modify main() in PS2_lib.py

--- PS2_lib.py
+++ PS2_lib.py
@@ -4,60 +4,28 @@ import time
 
 class PS2Gamepad:
     def __init__(self, dat, cmd, cs, clk, freq=250000):
-        # 'X1'  MISO=DAT(DI)
-        # 'X2'  MOSI=CMD(DO)
-        # 'X3'  CS=CS
-        # 'X4'  CLK=CLK
         self.__pinDAT = Pin(dat, Pin.IN, Pin.PULL_DOWN)
         self.__pinCMD = Pin(cmd, Pin.OUT)
         self.__pinCS = Pin(cs, Pin.OUT, Pin.PULL_UP)
         self.__pinCLK = Pin(clk, Pin.OUT)
         # This is the ID of the button
-        # SELECT     = 1
-        # L3         = 2
-        # R3         = 3
-        # START      = 4
-        # PAD_UP     = 5
-        # PAD_RIGHT  = 6
-        # PAD_DOWN   = 7
-        # PAD_LEFT   = 8
-        # L2         = 9
-        # R2         = 10
-        # L1         = 11
-        # R1         = 12
-        # TRIANGLE   = 13
-        # CIRCLE     = 14
-        # CROSS      = 15
-        # SQUARE     = 16
+        # SELECT   = 1   L3        = 2   R3       = 3   START    = 4
+        # PAD_UP   = 5   PAD_RIGHT = 6   PAD_DOWN = 7   PAD_LEFT = 8
+        # L2       = 9   R2        = 10  L1       = 11  R1       = 12
+        # TRIANGLE = 13  CIRCLE    = 14  CROSS    = 15  SQUARE   = 16
         self.__buttonMask = (
-            "SELECT",
-            "L3",
-            "R3",
-            "START",
-            "PAD_UP",
-            "PAD_RIGHT",
-            "PAD_DOWN",
-            "PAD_LEFT",
-            "L2",
-            "R2",
-            "L1",
-            "R1",
-            "TRIANGLE",
-            "CIRCLE",
-            "CROSS",
-            "SQUARE"
+            "SELECT", "L3", "R3", "START",
+            "PAD_UP", "PAD_RIGHT", "PAD_DOWN", "PAD_LEFT",
+            "L2", "R2", "L1", "R1",
+            "TRIANGLE", "CIRCLE", "CROSS", "SQUARE"
         )
-        # 0x01 start Communicating
-        # 0x42 get btn data
-        # 0x43 enter/exit config mod
-        # 0x44 set Analog Mode
-        # 0x4D set Vibration Mode
-        self.cmdList = (0x01, 0x42, 0x43, 0x44, 0x4D)
         self.__sendBuf = [0x01, 0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
         self.__dataBuf = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
         self.__freq = int(1000000 / freq)
-        self.__shockValue = [0x00, 0x00]
         self.redMode = False
+        self.configMode = False
+        self.vibMode = False
+        self.__vibValue = [0x00, 0x00]
         self.setReady()
 
     def __ready2send(self):
@@ -69,7 +37,7 @@ class PS2Gamepad:
     def __sendFrame(self, cmd=0):
         ret = 0
         for respBit in (1, 2, 4, 8, 16, 32, 64, 128):
-            if(respBit & cmd):
+            if(respBit & cmd):  # send cmd
                 self.__pinCMD.value(1)
             else:
                 self.__pinCMD.value(0)
@@ -77,7 +45,7 @@ class PS2Gamepad:
             time.sleep_us(self.__freq)
             self.__pinCLK.value(0)
             time.sleep_us(self.__freq)
-            if(self.__pinDAT.value() == 1):
+            if(self.__pinDAT.value() == 1):  #get resp
                 ret = respBit | ret
             self.__pinCLK.value(1)
         return ret
@@ -103,19 +71,81 @@ class PS2Gamepad:
         for i in range(0, 6, 1):
             self.__dataBuf[i] = self.__sendFrame(self.__sendBuf[i + 3])
         self.__endOfsend()
+        time.sleep_ms(10)
 
-    def ConfigMode(self, enable=True):
-        if enable is True:
-            self.__sendBuf[1]
+    def setConfig(self):
+        self.clearSendBuf()
+        self.clearDataBuf()
+        self.__sendBuf[1] = 0x43
+        if self.configMode is False:
+            # start config
+            self.__sendBuf[3] = 0x01
+            self.configMode = True
+        else:
+            # save config
+            for index in range(4, 9, 1):
+                self.__sendBuf[index] = 0x5A
+            self.configMode = False
+        self.__ready2send()
+        self.cmdWithPad()
+        self.__endOfsend()
+        time.sleep_ms(10)
 
-    def setShockValue(self, WW, YY):
+    def setAnalogMode(self, red=False, lock=False):
+        self.clearSendBuf()
+        self.clearDataBuf()
+        self.__sendBuf[1] = 0x44
+        if red is False:
+            self.__sendBuf[3] = 0x00  # disable red mode
+        else:
+            self.__sendBuf[3] = 0x01  # enable red mode
+        if lock is False:
+            # unlock the setting, you can use <mode> btn to change analog
+            self.__sendBuf[4] = 0xEE
+        else:
+            # lock the setting, you can' use <mode> btn to change analog
+            self.__sendBuf[4] = 0x03
+        self.__ready2send()
+        self.cmdWithPad()
+        self.__endOfsend()
+        time.sleep_ms(10)
+
+    def setVibMode(self, vibration=False):
+        self.clearSendBuf()
+        self.clearDataBuf()
+        self.__sendBuf[1] = 0x4D
+        if vibration is False:
+            # enable vibration
+            self.__sendBuf[4] = 0X00
+            self.vibMode = False
+        else:
+            # disable vibration
+            self.__sendBuf[4] = 0X01
+            self.vibMode = True
+        self.__ready2send()
+        self.cmdWithPad()
+        self.__endOfsend()
+        time.sleep_ms(10)
+
+    # set vibration value
+    def setVibValue(self, WW, YY):
+        self.clearSendBuf()
+        self.clearDataBuf()
         if 0x00 <= WW <= 0xFF:
-            self.__shockValue[0] = WW
+            self.__vibValue[0] = WW
         if 0x40 <= YY <= 0xFF:
-            self.__shockValue[1] = YY
+            self.__vibValue[1] = YY
+        self.__ready2send()
+        self.cmdWithPad()
+        self.__endOfsend()
+        time.sleep_ms(10)
 
     def getBtn(self):
+        self.clearSendBuf()
         self.clearDataBuf()
+        if self.vibMode is True:
+            self.__dataBuf[3] = self.__vibValue[0]
+            self.__dataBuf[4] = self.__vibValue[1]
         self.cmdWithPad()
         retList = []
         btn = (self.__dataBuf[1] << 8) | self.__dataBuf[0]
@@ -143,16 +173,24 @@ class PS2Gamepad:
 
 def main():
     time.sleep_ms(1000)
-    ps2 = PS2Gamepad(freq=250000, dat='X1', cmd='X2', cs='X3', clk='X4')
+    # 'X1':  MISO=DAT(DI)
+    # 'X2':  MOSI=CMD(DO)
+    # 'X3':  CS=CS
+    # 'X4':  CLK=CLK
+    ps2 = PS2Gamepad(freq=1000000, dat='X1', cmd='X2', cs='X3', clk='X4')
     input("please enter to start debug")
-    ps2.setShockValue(WW=0xFF, YY=0xFF)
+    ps2.setConfig()
+    ps2.setAnalogMode(red=True, lock=True)
+    ps2.setVibMode(vibration=True)
+    ps2.setConfig()
+    ps2.setVibValue(0xFF, 0xFF)
     while(True):
         btn = ps2.getBtn()
         if btn:
             for i in range(0, len(btn)):
                 btn[i] = ps2.getBtnName(btn[i])
             print("get button:" + str(btn))
-        time.sleep_ms(50)
+        time.sleep_ms(100)
 
 
 if __name__ == "__main__":
